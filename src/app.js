fd388c503e7159e0d0bc586a7d5427dc3dad40c3
vaibhavInnovation <vaibhav.ayushgupta@gmail.com>
2025-02-13 11:44:10 +0530

    uploading all files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+let loader=document.querySelector(".loader");
+let content=document.querySelector(".content");
+content.style.display='none';
+
+let loadContent=()=>{
+   
+    setTimeout(()=>{
+        loader.style.display='none';
+        content.style.display='block';
+    },2000);
+}
+
+loadContent();
+
+let check=()=>{
+    let number=document.querySelector(".mob");
+    if(number.value.length!=10){
+        alert("Please enter valid mobile number");
+    }
+}
